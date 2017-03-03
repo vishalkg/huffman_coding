638a8deb0bf355a1e5ed8c36c0623f8d30c73410
Vishal Gupta <vishal.gupta4081@gmail.com>
2017-03-03 17:28:34 -0500

    optimized freq_table construction, cleanup

--- src/huffman_code/Gen_huffman_code.java
+++ src/huffman_code/Gen_huffman_code.java
@@ -4,13 +4,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Gen_huffman_code {
 
 	public static void gen_codes(Node n, String code){
 		//System.out.println("Curr Node Freq: "+n.get_freq());
 		if (n.get_msg() != -1){
-			System.out.println(n.get_msg()+"==>"+code);
+			//System.out.println(n.get_msg()+"==>"+code);
 			return;
 		}
 		else{
@@ -19,14 +20,15 @@ public class Gen_huffman_code {
 		}
 	}
 	
-	public static void build_tree_using_binary_heap(int[][] freq_table) {
+	public static void build_tree_using_binary_heap(int[] freq_table) {
 		
 		/** Initially create a min heap with single node trees **/
 		BinaryHeap min_heap = new BinaryHeap(freq_table.length);
 		for (int i=0;i<freq_table.length;i++){
-			Node n = new Node(freq_table[i][0], freq_table[i][1]);
-			min_heap.insert(n);
-			//System.out.println(min_heap.get_root().get_freq());
+			if (freq_table[i]!=0){
+				Node n = new Node(freq_table[i], i);
+				min_heap.insert(n);
+			}
 		}
 		
 		/** Now we have a min heap of trees having single nodes only	**
@@ -36,10 +38,12 @@ public class Gen_huffman_code {
 			internal.set_left(min_heap.del_min());internal.get_left().set_parent(internal);
 			internal.set_right(min_heap.del_min());internal.get_right().set_parent(internal);
 			internal.set_freq(internal.get_left().get_freq()+internal.get_right().get_freq());
+			/**
 			System.out.println(internal.get_left().get_freq()+
 					":"+internal.get_left().get_msg()+
 					"|"+internal.get_right().get_freq()+
 					":"+internal.get_right().get_msg());
+			**/
 			min_heap.insert(internal);
 		}
 		String code = "";
@@ -51,49 +55,37 @@ public class Gen_huffman_code {
 		/*****************************************************/
 		/**Read the input file and build the frequency table**/
 		File freq_data = null;
-		if (args.length == 0)
-			System.out.println("Input not provided .. terminating");
-		else
-			freq_data = new File(args[0]);
+		if (args.length == 0){
+			System.out.println("Input not provided ... terminating");
+			return;
+		}
+		freq_data = new File(args[0]);
 		BufferedReader reader = new BufferedReader(new FileReader(freq_data));
-		ArrayList<Integer> freq = new ArrayList<Integer>();
-		ArrayList<Integer> values = new ArrayList<Integer>();
+		int[] freq_table = new int[20000000];
+		Arrays.fill(freq_table, 0);
 		String line;
+		System.out.print("Reading input file ... ");
 		while ((line = reader.readLine())!=null) {
 			if (!line.equals("")){
 				int next_value = Integer.parseInt(line.toString());
-				if (values.contains(next_value)) {
-					int index = values.indexOf(next_value);
-					freq.set(index, freq.get(index)+1);
-				}
-				else {
-					values.add(next_value);
-					freq.add(1);
-				}
+				freq_table[next_value]++;
 			}
 		}
-		int freq_table[][] = new int[freq.size()][2];
-		for (int i=0;i<freq.size();i++) {
-			freq_table[i][0] = freq.get(i).intValue();
-			freq_table[i][1] = values.get(i).intValue();
-		}
-		
-		/*
-		Arrays.sort(freq_table, (a,b)->Double.compare(a[0],b[0]));
-		for (int i=0;i<freq_table.length;i++)
-			System.out.println(freq_table[i][1]+"=>"+freq_table[i][0]);
-		*/
-		
+		System.out.print("Done.\n");
+				
 		long startTime,endTime;
 		/****************** Binary Heap ******************/
 		{
+			System.out.print("Building Huffman Tree using Binary Heap ... \nTrial:");
 			startTime = System.currentTimeMillis();
-			for (int i=0;i<1;i++) {
+			for (int i=0;i<10;i++) {
+				System.out.print((i+1)+".. ");
 				build_tree_using_binary_heap(freq_table);
 			}
+			System.out.print(" Done\n");
 			endTime   = System.currentTimeMillis();
 			double totalTime_BH = (endTime - startTime)/10;
-			System.out.println(totalTime_BH);
+			System.out.println("Average Time: "+totalTime_BH);
 		}
 		
 		final boolean DEBUG = false;
